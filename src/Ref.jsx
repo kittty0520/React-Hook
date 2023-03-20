6123fb5cf93bf4434a0ea866e676e3832a4ffbf3
kittty0520 <dheud1004@gmail.com>
2023-03-20 17:26:03 +0900

    Difference variable and useRef

--- src/Ref.jsx
+++ src/Ref.jsx
@@ -1,23 +1,31 @@
 import React, { useRef, useState } from 'react';
 
 export default function Ref() {
-	const [count, setCount] = useState(0);
+	const [renderer, setRenderer] = useState(0);
 	const countRef = useRef(0);
+	let countVar = 0;
 
 	console.log('렌더링됨');
 
-	const increaseCountState = () => {
-		setCount(count + 1);
+	const doRendering = () => {
+		setRenderer(renderer + 1);
+	};
+
+	const increaseCountVar = () => {
+		countVar = countVar + 1;
+		console.log('Var:', countVar);
 	};
 	const increaseCountRef = () => {
 		countRef.current = countRef.current + 1;
 		console.log('Ref:', countRef.current);
 	};
+
 	return (
 		<div>
-			<p>State: {count}</p>
+			<p>Var: {countVar}</p>
 			<p>Ref: {countRef}</p>
-			<button onClick={increaseCountState}>State Up</button>
+			<button onClick={doRendering}>Rerendering</button>
+			<button onClick={increaseCountVar}>State Up</button>
 			<button onClick={increaseCountRef}>Ref Up</button>
 		</div>
 	);
